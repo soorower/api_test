70b48471b4e5dfc970b774eb357a25caa5ecf7b4
soorower <sorowerhossan01@gmail.com>
2022-01-24 22:48:25 +0600

    make it better

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import request
 import flask
+import requests
+from bs4 import BeautifulSoup as bs
 app = flask.Flask(__name__)
 
 @app.route('/',methods = ['GET'])
 
 def home():
   arg = request.args['arg']
-  return 'Hello World'
+  r = requests.get(arg)
+  soup = bs(r.content,'html.parser')
+  Product_name = str(soup)[10:29]
+  
+  return Product_name
