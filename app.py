6b6c3b11318886d49a6d6476de268c4058b704c5
soorower <sorowerhossan01@gmail.com>
2022-01-25 00:17:12 +0600

    make it better

--- app.py
+++ app.py
@@ -32,42 +32,42 @@ def home():
 
   check = request.args['token']
   link1 = request.args['link']
-#   link2 = link1.split('/')[-1]
-#   link = f'https://api.takealot.com/rest/v-1-10-0/product-details/{link2}?platform=desktop'
-#   if check=='Jx8SeUYilsP0h2000':
-#     r = requests.get(link,headers=headers)
-#     soup = bs(r.content,'html.parser')
+  link2 = link1.split('/')[-1]
+  link = f'https://api.takealot.com/rest/v-1-10-0/product-details/{link2}?platform=desktop'
+  if check=='Jx8SeUYilsP0h2000':
+    r = requests.get(link,headers=headers)
+    soup = bs(r.content,'html.parser')
 
 
-#     if '"title":' in str(soup)[:50]:
+    if '"title":' in str(soup)[:50]:
 
-#       data = json.loads(str(soup))
-#       product_name= data.get('title')
-#       price = data.get('buybox').get('prices')[0]
-#       stock = data.get('stock_availability').get('status')
+      data = json.loads(str(soup))
+      product_name= data.get('title')
+      price = data.get('buybox').get('prices')[0]
+      stock = data.get('stock_availability').get('status')
 
-#       images = []
-#       if type(data.get('gallery').get('images'))==list:
-#           for i in data.get('gallery').get('images'):
-#               images.append(i.replace('{size}','zoom'))
-#       else:
-#           images = data.get('gallery').get('images')
-#       data_set = {
-#           'Product Name': product_name,
-#           'Price': price,
-#           'Stock Availability': stock,
-#           'Images': images
-#       }
+      images = []
+      if type(data.get('gallery').get('images'))==list:
+          for i in data.get('gallery').get('images'):
+              images.append(i.replace('{size}','zoom'))
+      else:
+          images = data.get('gallery').get('images')
+      data_set = {
+          'Product Name': product_name,
+          'Price': price,
+          'Stock Availability': stock,
+          'Images': images
+      }
 
-#       jsons = json.dumps(data_set)
-#     else:
-#         data_set = {
-#             'Error': 'Something Went Wrong, try after some seconds'
-#         }
-#         jsons = json.dumps(data_set)
-#   else:
-#       data_set = {
-#             'TOKEN ERROR': 'INVALID TOKEN'
-#         }
-#       jsons = json.dumps(data_set)
-  return 'hello'
+      jsons = json.dumps(data_set)
+    else:
+        data_set = {
+            'Error': 'Something Went Wrong, try after some seconds'
+        }
+        jsons = json.dumps(data_set)
+  else:
+      data_set = {
+            'TOKEN ERROR': 'INVALID TOKEN'
+        }
+      jsons = json.dumps(data_set)
+  return jsons
